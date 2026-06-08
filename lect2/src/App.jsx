d7317aa4138b1fff7d65543bf1bd6f57c795185d
Anurag1707 <Anuragjhariya1707@gmail.com>
2026-06-08 22:46:01 +0530

    context

--- lect2/src/App.jsx
+++ lect2/src/App.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import Todo from './Todo'
+// import React from 'react'
+// import Todo from './Todo'
 
-const App = () => {
-  return (
-    <div>
-      <Todo/>
-    </div>
-  )
-}
+// const App = () => {
+//   return (
+//     <div>
+//       <Todo/>
+//     </div>
+//   )
+// }
 
-export default App
+// export default App
 
 // import React from 'react'
 // import  Cchange  from './Cchange'
@@ -39,3 +39,55 @@ export default App
 // }
 
 // export default App
+
+// import React from 'react'
+// import Context from './Context'
+// import Home from './Home'
+
+// export const App = () => {
+//   return (
+//     <div>
+//       <Home/>
+//     </div>
+//   )
+// }
+
+
+//  export default App
+
+
+
+import { useEffect } from "react"
+import { useState } from "react"
+
+import React from 'react'
+import Home from "./Home"
+
+//main-> app-> A-> b-> C
+
+
+
+import NavBar from "./NavBar"
+
+import { Route, Routes } from "react-router-dom"
+import Cart from "./Cart"
+
+const App = () => {
+  let [apiData,SetApiData] =   useState([])
+ let [cart,SetCart]= useState([])
+  return (
+
+    <div>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home apiData={apiData}  SetApiData={SetApiData} cart={cart}  SetCart={SetCart}/>}/>
+        <Route path="/cart" element={<Cart cart={cart}/>}/>
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App
+
+
---
+++ lect2/src/Home.jsx
@@ -0,0 +1,54 @@
+// import React, { useContext } from 'react'
+// import Context from './Context'
+
+// const Home = () => {
+//     let data = useContext(Context)
+//     console.log(data)
+//   return (
+//     <div>Home</div>
+//   )
+// }
+
+// export default Home
+
+import React, { useEffect, useState } from 'react'
+import './App.css'
+const Home = ({apiData,SetApiData,cart,SetCart}) => {
+
+  console.log(cart,"carttttt");
+  
+     
+
+    
+  useEffect(()=>{
+   async function apiCall(){
+
+  let res=   await   fetch("https://dummyjson.com/products")
+       let data=       await res.json()
+       console.log(data);
+       SetApiData(data.products)
+       
+
+    }
+    apiCall()
+
+  },[])
+
+  return (
+    <div id='card'>
+      {
+        apiData.map((a)=>{
+          return(<div id='main_card'>
+            <h6>{a.price}</h6>
+          <img   src={a.thumbnail}/>
+          {/* <h4>{a.title}</h4> */}
+          
+          <button onClick={()=>SetCart([...cart,a])}>add</button>
+          </div>)
+        })
+      }
+    </div>
+  )
+}
+
+export default Home
